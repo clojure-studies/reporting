1bdf956bc9f6a814bb32f7f565e398615cae4996
Yury Kotov <bairkan@yandex.ru>
2015-07-15 17:21:58 +0300

    Database access

--- src/reporting/models/db.clj
+++ src/reporting/models/db.clj
@@ -10,3 +10,16 @@
                    :db database-url})
 
 (def db {:connection-uri database-url})
+
+(defn seed-employees []
+  (sql/insert! db :employees
+    {:name "Albert Einstein" :occupation "Engineer" :place "Ulm" :country "Germany"}
+    {:name "Alfred Hitchcock" :occupation "Movie Director" :place "London" :country "UK"}
+    {:name "Wernher Von Braun" :occupation "Rocket Scientist" :place "Wyrzysk" :country "Poland"}
+    {:name "Sigmund Freud" :occupation "Neurologist" :place "Pribor" :country "Czech Republic"}
+    {:name "Mahatma Gandhi" :occupation "Lawyer" :place "Gujarat" :country "India"}
+    {:name "Sachin Tendulkar" :occupation "Cricket Player" :place "Mumbai" :country "India"}
+    {:name "Michael Schumacher" :occupation "F1 Racer" :place "Cologne" :country "Germany"}))
+
+(defn read-employees []
+  (sql/query db ["SELECT * FROM employees"]))
